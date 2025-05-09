9a13f4db6e10cfc41a9047624c34bf3cca77c64a
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2025-05-09 08:00:01 -0400

    Second modification to the bash.sh file

--- bash.sh
+++ bash.sh
@@ -13,3 +13,17 @@ if [ ! -d "$repo_direcory" ]; then
 	echo "creating directory: $repo_directory"
 	mkdir "$repo_directory"
 fi
+
+cd "$repo_directory" || exit
+
+# Initialize a new git repository
+if [ ! -d ".git" ]; then
+	git init
+fi
+
+# Add a remote repository and fetch the latest changes
+git_repo_url = "https://github.com/example.git #replace with your url
+git remote add origin "$git_repo_url"
+git fetch origin
+
+echo "repository setup complete. You can start working on the $repo_directory directory"
